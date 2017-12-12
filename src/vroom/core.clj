66a76c1e5d29b80e0043921d3475bd404ff2f4df
TeleBooth <jbooth1@terpmail.umd.edu>
2017-12-11 23:26:12 -0500

    Buffer and polling adjusted for GPS

--- src/vroom/core.clj
+++ src/vroom/core.clj
@@ -74,7 +74,7 @@
 						(reset! buffers-atom []))]
 
     (go-loop [i 0]
-      (<! (async/timeout 10000))
+      (<! (async/timeout 500))
       (when @broadcast-enabled?_ (broadcast! i))
       (recur (inc i)))))
 
@@ -120,12 +120,13 @@
 
     (reset! web-server_ stop-fn)))
 
-(defn -main [serial-port-number]
+(defn -main
+	[serial-port-number]
 	"A very simple web server using Ring & Jetty"
 	;;starts the socket server
 	(start-web-server!)
-	;;starts broadcasting
-	(start-gps-broadcaster!)
 	;;starts listening on the serial port and passing it to the buffer
-	(def serial-port (serial/open serial-port-number :baud-rate 4800))
-	(serial/listen! serial-port (fn [stream] (swap! buffers-atom concat (exhaust-stream stream n)))))
+	(def serial-port (serial/open serial-port-number :baud-rate 9600))
+	(serial/listen! serial-port (fn [stream] (swap! buffers-atom concat (exhaust-stream stream n))))
+	;;starts broadcasting
+	(start-gps-broadcaster!))
